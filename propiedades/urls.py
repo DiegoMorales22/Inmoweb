b3b1db02e667025de01aec182fa45042e19210a6
Diego Morales <diegomorales.olarte@gmail.com>
2025-11-05 09:55:16 -0500

    cree mis vistas para editar/eliminar  las propiedades con exito

--- propiedades/urls.py
+++ propiedades/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.lista_propiedades, name='propiedades'),
     path('agentes/',views.lista_Agentes, name='agentes'),
     path('registroPropiedad/',views.RegistroPropiedad, name='registroPropiedad'),
-    path('registroExitoso/',views.registroExitoso, name='registroExitoso') 
+    path('registroExitoso/',views.registroExitoso, name='registroExitoso'), 
+    path('editar/<int:id>/',views.editar_propiedad, name='editar_propiedad'),
+    path('eliminar/<int:id>/', views.eliminar_propiedad, name='eliminar_propiedad')
 ]
--- propiedades/views.py
+++ propiedades/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404
+
 from .models import Propiedad, Agente
 from django.http import HttpResponse
 from .forms import RegistrarPropiedad
+
 # Create your views here.
 
 def lista_propiedades(request):
@@ -25,6 +28,23 @@ def RegistroPropiedad(request):
 
 def registroExitoso(request):
     return render(request,'propiedades/registroexitoso.html')
+
+def editar_propiedad(request,id):
+    propiedad=get_object_or_404(Propiedad, id=id)
+    if request.method=='POST':
+          form = RegistrarPropiedad(request.POST,request.FILES,instance=propiedad)
+          if form.is_valid():
+               form.save()
+               return redirect('lista_propiedades')
+    else:
+         form=RegistrarPropiedad(instance=propiedad)
+    return render(request,'propiedades/editar_propiedad.html', {'form': form, 'propiedad': propiedad})
+
+def eliminar_propiedad(request,id):
+     propiedad = get_object_or_404(Propiedad, id=id)
+     propiedad.delete()
+     return redirect('lista_propiedades')
+          
 
 
 
